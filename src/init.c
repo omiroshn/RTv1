1328af77159e6cd03c0ed5fca6714c3d1022ce00
omiroshn <alexey.miroshnik1@gmail.com>
2018-03-18 20:36:21 +0200

    parsing

--- src/init.c
+++ src/init.c
@@ -30,83 +30,9 @@ void	init(t_map *m)
 void	init_geometry(t_map *m)
 {
 	m->flag = 0;
-
-	//* to parse
-	
-	
-
-	m->geom.obj[0].name = SPHERE;
-	m->geom.obj[0].center = (t_vec3){0, 0.99, 2};
-	m->geom.obj[0].color = (t_vec3){255, 0, 0};
-	m->geom.obj[0].specular = 50;
-	m->geom.obj[0].radius = 1;
-	m->geom.obj[0].reflection = 0.3;
-	
-	m->geom.obj[1].name = SPHERE;
-	m->geom.obj[1].center = (t_vec3){-2, 0.99, 3};
-	m->geom.obj[1].color = (t_vec3){0, 0, 255};
-	m->geom.obj[1].specular = 50;
-	m->geom.obj[1].radius = 1;
-	m->geom.obj[1].reflection = 0.3;
-	
-	m->geom.obj[2].name = SPHERE;
-	m->geom.obj[2].center = (t_vec3){2, 0.99, 3};
-	m->geom.obj[2].color = (t_vec3){0, 255, 0};
-	m->geom.obj[2].specular = 50;
-	m->geom.obj[2].radius = 1;
-	m->geom.obj[2].reflection = 0.3;
-	
-	m->geom.obj[3].name = CONE;
-	m->geom.obj[3].center = (t_vec3){-2, 1, 0};
-	m->geom.obj[3].direction = (t_vec3){-2, 0, 0};
-	m->geom.obj[3].color = (t_vec3){0, 0, 255};
-	m->geom.obj[3].specular = 1000;
-	m->geom.obj[3].radius = 1;
-	m->geom.obj[3].reflection = 0.3;
-	m->geom.obj[3].angle = 30;
-
-	m->geom.obj[4].name = CYLINDER;
-	m->geom.obj[4].center = (t_vec3){2, 0, 0};
-	m->geom.obj[4].direction = (t_vec3){2, 1, 0};
-	m->geom.obj[4].color = (t_vec3){0, 255, 0};
-	m->geom.obj[4].specular = 1000;
-	m->geom.obj[4].radius = 0.5;
-	m->geom.obj[4].reflection = 0.3;
-
-	m->geom.obj[5].name = PLANE;
-	m->geom.obj[5].center = (t_vec3){0, 0, 0};
-	m->geom.obj[5].color = (t_vec3){255, 255, 255};
-	m->geom.obj[5].direction = (t_vec3){0, 1, 0};
-	m->geom.obj[5].specular = 1000;
-	m->geom.obj[5].reflection = 0.5;
-
-	m->geom.light[0].type = AMBIENT;
-	m->geom.light[0].intensity = 0.2;
-	m->geom.light[0].position = (t_vec3){0, 0, 0};
-	m->geom.light[0].direction = (t_vec3){0, 0, 0};
-
-	m->geom.light[1].type = POINT;
-	m->geom.light[1].intensity = 0.6;
-	m->geom.light[1].position = (t_vec3){2, 0, 0.5};
-	m->geom.light[1].direction = (t_vec3){0, 0, 0};
-
-	m->geom.light[2].type = DIRECTIONAL;
-	m->geom.light[2].intensity = 0.2;
-	m->geom.light[2].position = (t_vec3){0, 0, 0};
-	m->geom.light[2].direction = (t_vec3){1, 4, 4};
-
-	m->geom.light[3].type = POINT;
-	m->geom.light[3].intensity = 0.6;
-	m->geom.light[3].position = (t_vec3){-2, 1, 0.5};
-	m->geom.light[3].direction = (t_vec3){0, 0, 0};
-	//*
-
-	//*viewport
 	m->vp.w = 1.0f;
 	m->vp.h = 1.0f;
 	m->vp.dist = 1.0f;
-	//*
-
 	m->geom.t_min = 0.001f;
 	m->geom.t_max = INFINITY;
 }
--- src/parse.c
+++ src/parse.c
@@ -59,82 +59,153 @@ char		*get_from_brackets(char *str, char c1, char c2)
 	return (tmp);
 }
 
+float		ft_ftoi(char *str)
+{
+	int		i;
+	int		len;
+	float	tmp;
+	float	tmp2;
+
+	i = -1;
+	len = 0;
+	tmp = ft_atoi(str);
+	if (!(ft_strchr(str, '.')))
+		return (tmp);
+	while (str[++i] != '.')
+		len++;
+	tmp2 = ft_atoi(ft_strsub(str, len + 1, ft_strlen(str)));
+	tmp2 /= pow(10, ft_strlen(str) - len - 1);
+	tmp += tmp2;
+	return (tmp);
+}
+
+/* ************************************************************************** */
+
+void		read_camera(t_map *m, int fd, char *lol, char **numbers)
+{
+	char	**parameters;
+	char	**coordinates;
+
+	get_next_line(fd, &lol) != 1 ? put_error("map invalid.") : 0;
+	!ft_strlen(lol) ? put_error("invalid map.") : 0;
+	numbers = ft_strsplit(lol, ' ');
+	if (ft_strcmp(numbers[0], "camera"))
+		put_error("camera name.");
+	numbers[1] = get_from_brackets(numbers[1], '[', ']');
+	if (ft_countwords(numbers[1], ',') != 2)
+		put_error("empty numbers.");
+	parameters = ft_strsplit(numbers[1], ',');
+	parameters[0] = get_from_brackets(parameters[0], '{', '}');
+	parameters[1] = get_from_brackets(parameters[1], '{', '}');
+	if (ft_countwords(parameters[0], ';') != 3
+				|| ft_countwords(parameters[1], ';') != 3)
+		put_error("empty parameter.");
+	coordinates = ft_strsplit(parameters[0], ';');
+	m->geom.O.x = ft_ftoi(coordinates[0]);
+	m->geom.O.y = ft_ftoi(coordinates[1]);
+	m->geom.O.z = ft_ftoi(coordinates[2]);
+	coordinates = ft_strsplit(parameters[1], ';');
+	m->geom.camera_rot.x = deg_to_rad(ft_ftoi(coordinates[0]));
+	m->geom.camera_rot.y = deg_to_rad(ft_ftoi(coordinates[1]));
+	m->geom.camera_rot.z = deg_to_rad(ft_ftoi(coordinates[2]));
+}
+
+/* ************************************************************************** */
+
+void		read_objects(t_map *m, char	*objects, int i)
+{
+	char	**parameters;
+	char	**coordinates;
+
+	if (ft_countwords(objects, ',') != 8)
+		put_error("empty numbers.");
+	parameters = ft_strsplit(objects, ',');
+	m->geom.obj[i].name = ft_atoi(parameters[0]);
+	if (ft_countwords(parameters[1], ';') != 3
+			|| ft_countwords(parameters[2], ';') != 3
+			|| ft_countwords(parameters[3], ';') != 3)
+		put_error("empty parameter.");
+	parameters[1] = get_from_brackets(parameters[1], '{', '}');
+	coordinates = ft_strsplit(parameters[1], ';');
+	m->geom.obj[i].center = (t_vec3){ft_ftoi(coordinates[0]),
+		ft_ftoi(coordinates[1]), ft_ftoi(coordinates[2])};
+	parameters[2] = get_from_brackets(parameters[2], '{', '}');
+	coordinates = ft_strsplit(parameters[2], ';');
+	m->geom.obj[i].direction = (t_vec3){ft_ftoi(coordinates[0]),
+		ft_ftoi(coordinates[1]), ft_ftoi(coordinates[2])};
+	parameters[3] = get_from_brackets(parameters[3], '{', '}');
+	coordinates = ft_strsplit(parameters[3], ';');
+	m->geom.obj[i].color = (t_vec3){ft_ftoi(coordinates[0]),
+		ft_ftoi(coordinates[1]), ft_ftoi(coordinates[2])};
+	m->geom.obj[i].specular = ft_ftoi(parameters[4]);
+	m->geom.obj[i].radius = ft_ftoi(parameters[5]);
+	m->geom.obj[i].reflection = ft_ftoi(parameters[6]);
+	m->geom.obj[i].angle = ft_ftoi(parameters[7]);
+}
+
+/* ************************************************************************** */
+
+void		read_lights(t_map *m, char *objects, int i)
+{
+	char	**parameters;
+	char	**coordinates;
+
+	if (ft_countwords(objects, ',') != 4)
+		put_error("empty numbers.");
+	parameters = ft_strsplit(objects, ',');
+	m->geom.light[i].type = ft_atoi(parameters[0]);;
+	m->geom.light[i].intensity = ft_ftoi(parameters[1]);
+	if (ft_countwords(parameters[2], ';') != 3
+			|| ft_countwords(parameters[3], ';') != 3)
+		put_error("empty parameter.");
+	parameters[2] = get_from_brackets(parameters[2], '{', '}');
+	coordinates = ft_strsplit(parameters[2], ';');
+	m->geom.light[i].position = (t_vec3){ft_ftoi(coordinates[0]),
+		ft_ftoi(coordinates[1]), ft_ftoi(coordinates[2])};
+	parameters[3] = get_from_brackets(parameters[3], '{', '}');
+	coordinates = ft_strsplit(parameters[3], ';');
+	m->geom.light[i].direction = (t_vec3){ft_ftoi(coordinates[0]),
+		ft_ftoi(coordinates[1]), ft_ftoi(coordinates[2])};
+}
+
+/* ************************************************************************** */
+
 void		read_map(t_map *m, char *filename)
 {
 	int		fd;
 	char	*lol;
 	char	**numbers;
-	char	**parameters;
-	char	**coordinates1;
-	char	**coordinates2;
 
 	check_filename(filename);
 	if ((fd = open(filename, O_RDONLY)) < 0 || read(fd, NULL, 0) < 0)
 		put_error("opening file.");
 	if (get_next_line(fd, &lol) != 1 || ft_countwords(lol, ' ') != 2)
 		put_error("map invalid.");
+	!ft_strlen(lol) ? put_error("invalid map.") : 0;
 	numbers = ft_strsplit(lol, ' ');
 	m->geom.objects = ft_atoi(numbers[0]);
 	m->geom.lights = ft_atoi(numbers[1]);
 	if (m->geom.objects <= 0 || m->geom.lights <= 0)
-		put_error("wrong input scene. U can't have less that 1 object or light.");
+		put_error("you can't have less that 1 object or light.");
 	m->geom.obj = malloc(sizeof(t_object) * m->geom.objects);
 	m->geom.light = malloc(sizeof(t_object) * m->geom.lights);
 	number_action(numbers, lol);
-	get_next_line(fd, &lol) != 1 ? put_error("map invalid.") : 0;
-	printf("%s\n", lol);
-	numbers = ft_strsplit(lol, ' ');
-	if (ft_strcmp(numbers[0], "camera"))
-		put_error("map invalid: camera name.");
-	numbers[1] = get_from_brackets(numbers[1], '[', ']');
-	printf("%s\n", numbers[0]);
-	printf("%s\n", numbers[1]);
-	if (ft_countwords(numbers[1], ',') != 2)
-		put_error("empty numbers.");
-	parameters = ft_strsplit(numbers[1], ',');
-	parameters[0] = get_from_brackets(parameters[0], '{', '}');
-	parameters[1] = get_from_brackets(parameters[1], '{', '}');
-	printf("%s\n", parameters[0]);
-	printf("%s\n", parameters[1]);
-	if (ft_countwords(parameters[0], ';') != 3 || ft_countwords(parameters[1], ';') != 3)
-		put_error("empty parameter.");
-	coordinates1 = ft_strsplit(parameters[0], ';');
-	printf("%s\n",coordinates1[0]);
-	printf("%s\n",coordinates1[1]);
-	printf("%s\n",coordinates1[2]);
-	m->geom.O.x = ft_atoi(coordinates1[0]);
-	m->geom.O.y = ft_atoi(coordinates1[1]);
-	m->geom.O.z = ft_atoi(coordinates1[2]);
-	printf("%f\n", m->geom.O.x);
-	printf("%f\n", m->geom.O.y);
-	printf("%f\n", m->geom.O.z);
-	coordinates2 = ft_strsplit(parameters[1], ';');
-	printf("%s\n", coordinates2[0]);
-	printf("%s\n", coordinates2[1]);
-	printf("%s\n", coordinates2[2]);
-	m->geom.camera_rot.x = deg_to_rad(ft_atoi(coordinates2[0]));
-	m->geom.camera_rot.y = deg_to_rad(ft_atoi(coordinates2[1]));
-	m->geom.camera_rot.z = deg_to_rad(ft_atoi(coordinates2[2]));
-	printf("%f\n", m->geom.camera_rot.x);
-	printf("%f\n", m->geom.camera_rot.y);
-	printf("%f\n", m->geom.camera_rot.z);
-	// ft_strcmp(numbers[0], "camera") ? put_error("map invalid.") : 0;
-	// printf("%s\n", parameters[0]);
-	// printf("%s\n", parameters[1]);
-	// !(ft_strcmp(numbers[0], "camera")) ? put_error("map invalid.") : 0;
+	read_camera(m, fd, lol, numbers);
 
-
-
-	// if (get_next_line(fd, &lol) != 1 || ft_countwords(lol, ' ') != 2)
-	// 	put_error("map invalid.");
-	// numbers = ft_strsplit(lol, ' ');
-	// m->pos.x = ft_atoi(numbers[0]) - 0.5;
-	// m->pos.y = ft_atoi(numbers[1]) - 0.5;
-	// number_action(numbers, lol);
-	// if (m->pos.x <= 1 || m->pos.x > m->karta.rows - 1
-	// 	|| m->pos.y <= 1 || m->pos.y > m->karta.cols - 1)
-	// 	put_error("invalid player position.");
-	// alloc_map(m, fd, lol, numbers);
-	// check_textures(m, m->karta.data);
-	// check_sprites(m, m->karta.data);
+	int o = 0;
+	int l = 0;
+	while (get_next_line(fd, &lol))
+	{
+		!ft_strlen(lol) ? put_error("invalid map.") : 0;
+		numbers = ft_strsplit(lol, ' ');
+		if (!(ft_strcmp(numbers[0], "object")))
+			read_objects(m, get_from_brackets(numbers[1], '[', ']'), o++);
+		else if (!(ft_strcmp(numbers[0], "light")))
+			read_lights(m, get_from_brackets(numbers[1], '[', ']'), l++);
+		else
+			put_error("wrong object or light name.");
+	}
+	if (o != m->geom.objects || l != m->geom.lights )
+		put_error("wrong number of objects or lights.");
+	close(fd);
 }
